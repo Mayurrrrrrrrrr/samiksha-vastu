1f4fec8f8a65d29e41966e317ae1d5aeecf4d4cc
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-04-23 21:13:40 +0530

    fix: hardcode oauth credentials

--- config/google_oauth.php
+++ config/google_oauth.php
@@ -2,8 +2,8 @@
 /**
  * Google OAuth Configuration
  */
-define('GOOGLE_CLIENT_ID', getenv('GOOGLE_CLIENT_ID') ?: '210283625210-hctu15a8808ccmejd2l20uvdi30ak6me.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', getenv('GOOGLE_CLIENT_SECRET') ?: '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');
+define('GOOGLE_CLIENT_ID', '210283625210-hctu15a8808ccmejd2l20uvdi30ak6me.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
 
 // Make sure this exactly matches what is configured in Google Cloud Console
 define('GOOGLE_REDIRECT_URI', 'https://samikshavastu.yuktaa.com/api/auth/google_callback');
